7fad08db8daf1c7706f9ea793d16decb08f9b0f0
kujiy <kujiy.fs@gmail.com>
2026-06-30 13:01:01 +0000

    Update multi-month-mini-calendar to 0.0.3

--- Casks/multi-month-mini-calendar.rb
+++ Casks/multi-month-mini-calendar.rb
@@ -1,6 +1,6 @@
 cask "multi-month-mini-calendar" do
-  version "0.0.2"
-  sha256 "7e960e581c6359d41b2d636d02fdb9dc5bae3a5e06d2b2bc21e405b7e6f617c4"
+  version "0.0.3"
+  sha256 "c353d07861715c067423cb987daf19f8678053fd5e3be87d8d45b2a347a53ff3"
 
   url "https://github.com/kujiy/multi-month-mini-calendar/releases/download/v#{version}/Multi-Month-Mini-Calendar.zip"
   name "Multi-Month Mini Calendar"
